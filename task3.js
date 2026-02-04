51c9287b76e793a487f705d965ff020ccbd6236c
caeseum <michaelcaeseum@gmail.com>
2026-02-04 18:42:25 +0300

    added a javascript file that i created a class.

---
+++ task3.js
@@ -0,0 +1,14 @@
+class communication {
+    constructor(question, answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+}
+    const Input = prompt("Input your question: ")
+    const chat=new communication(Input,"I dont know yet");
+    console.log(chat)
+    
+
+
+
+
